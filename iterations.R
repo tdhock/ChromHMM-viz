992bb4aee4b5f8a8ac9a34edadbe93dd3d0fe991
Toby Dylan Hocking <tdhock5@gmail.com>
2015-10-29 13:58:47 -0400

    read all iterations

--- iterations.R
+++ iterations.R
@@ -2,46 +2,62 @@ works_with_R("3.2.2",
              "tdhock/ggplot2@a8b06ddb680acdcdbd927773b1011c562134e4d2",
              data.table="1.9.6")
 
-emission.mat <- fread("Toby/emissions_10_999.txt")
-state.fac <- factor(emission.mat[[1]])
-
+emissions.txt.vec <- Sys.glob("toby/emissions.mat/*")
 emission.list <- list()
-for(experiment in names(emission.mat)[-1]){
-  frequency <- emission.mat[[experiment]]
-  emission.list[[experiment]] <-
-    data.table(experiment, state=state.fac, frequency)
+for(emissions.txt in emissions.txt.vec){
+  iteration.str <- sub(".*_i", "", emissions.txt)
+  iteration <- as.integer(iteration.str)
+  emission.mat <- fread(emissions.txt)
+  state.fac <- factor(emission.mat[[1]])
+  for(experiment in names(emission.mat)[-1]){
+    frequency <- emission.mat[[experiment]]
+    emission.list[[paste(emissions.txt, experiment)]] <-
+      data.table(iteration, experiment, state=state.fac, frequency)
+  }
 }
 emission <- do.call(rbind, emission.list)
-emission[, list(total=sum(frequency)), by=state]
-emission[, list(total=sum(frequency)), by=experiment]
 
 ggplot()+
   coord_equal()+
+  theme_bw()+
+  theme(panel.margin=grid::unit(0, "cm"))+
+  facet_wrap("iteration")+
   scale_fill_gradient(low="white", high="blue")+
   geom_tile(aes(state, experiment, fill=frequency),
             data=emission)
 
-transition.mat <- fread("Toby/transitions_10_999.txt", header=TRUE)
+transition.txt.vec <- Sys.glob("toby/transitions.mat/*")
 transition.list <- list()
-for(state in names(transition.mat)[-1]){
-  probability <- transition.mat[[state]]
-  state.to <- factor(state, levels(state.fac))
-  state.from <- factor(transition.mat[[1]], levels(state.fac))
-  transition.list[[state]] <-
-    data.table(state.to, state.from, probability)
+for(transition.txt in transition.txt.vec){
+  iteration.str <- sub(".*_i", "", transition.txt)
+  iteration <- as.integer(iteration.str)
+  transition.mat <- fread(transition.txt, header=TRUE)
+  for(state in names(transition.mat)[-1]){
+    probability <- transition.mat[[state]]
+    state.to <- factor(state, levels(state.fac))
+    state.from <- factor(transition.mat[[1]], levels(state.fac))
+    transition.list[[paste(transition.txt, state)]] <-
+      data.table(iteration, state.to, state.from, probability)
+  }
 }
 transition <- do.call(rbind, transition.list)
 
 ggplot()+
   coord_equal()+
+  theme_bw()+
+  theme(panel.margin=grid::unit(0, "cm"))+
+  facet_wrap("iteration")+
   scale_fill_gradient(low="white", high="blue")+
   geom_tile(aes(state.to, state.from, fill=probability),
             data=transition)
 
-metric.mat <- fread("Toby/iteration-loglike.txt", na.strings="-")
+metric.mat <- read.table(
+  "toby/iterations/iterations.txt",
+  header=TRUE, na.strings="-")
+metric.mat$log.neg.log.lik <- log(-metric.mat$EstimatedLogLikelihood)
 metrics.list <- list()
 for(metric.name in names(metric.mat)[-1]){
-  sub.mat <- metric.mat[-(1:2),]
+  sub.mat <- metric.mat#[-(1:2),]
   metrics.list[[metric.name]] <-
     data.table(metric.name,
                metric.value=sub.mat[[metric.name]],
